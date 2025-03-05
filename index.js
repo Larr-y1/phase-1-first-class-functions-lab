57c34050b1755370010c711ea80c2b224276cadd
Larry Otieno <larryotieno51@gmail.com>
2025-03-06 00:41:49 +0300

    Adds first class functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// Return the first two drivers from an array (non-destructive)
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Return the last two drivers from an array (non-destructive)
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing both functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function that creates a multiplier function
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Fare doubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Fare tripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function that selects different drivers based on the function passed
+  const selectDifferentDrivers = function(arrayOfDrivers, driverFunction) {
+    return driverFunction(arrayOfDrivers);
+};
